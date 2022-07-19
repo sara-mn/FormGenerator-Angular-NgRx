ebd5985ec9c9b07fa48d89da5810cfe694796773
saramn <smohaqeqn@gmail.com>
2022-07-19 15:13:33 +0430

    error message directive

--- src/app/services/validate.form.service.ts
+++ src/app/services/validate.form.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +9,5 @@ export class ValidateFormService {
   constructor() {
   }
 
-  // validateAllControls(formGroup: FormGroup) {
-  //   Object.keys(formGroup.controls).forEach(field => {
-  //     const control = formGroup.get(field);
-  //     if (control !== null) {
-  //       if (control instanceof FormControl) {
-  //         let k = control as FormControl<any>;
-  //         if(k.hasError('required'))
-  //           k.setErrors(Validators.required);
-  //       } else if (control instanceof FormGroup) {
-  //         this.validateAllControls(control);
-  //       }
-  //     }
-  //   });
-  // }
+  
 }
