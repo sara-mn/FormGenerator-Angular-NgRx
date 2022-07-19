c4346122e37681add51339f5788a73759c93fa16
saramn <smohaqeqn@gmail.com>
2022-07-19 15:31:54 +0430

    error message directive

--- src/app/services/validate.form.service.ts
+++ src/app/services/validate.form.service.ts
@@ -8,6 +8,13 @@ export class ValidateFormService {
 
   constructor() {
   }
-
+  validateAllControls(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control !== null) {
+        control.markAsTouched({ onlySelf: true });
+      }
+    });
+  }
 
 }
